db9cc6a694f6a5b801c7d0f7a7c7f2247ca67b19
yuling <yulinglin@protonmail.com>
2022-01-17 16:19:44 +0800

    refactor: Set the betting logic in MakeBet.js and change the horse betting data type in HorsrInfo.js

--- src/component/HorseInfo.js
+++ src/component/HorseInfo.js
@@ -1,39 +1,44 @@
 import React from 'react';
 import { useState } from 'react';
-import { Betting } from './Betting';
+import { MakeBet } from './MakeBet';
 
 function HorseInfo() {
-  const [bettingOdds, setBettingOdds] = useState(2);
+  const [bettingOdds, setBettingOdds] = useState([
+    { name: 'Sprite', bettingOdds: 2 },
+    { name: 'Thene', bettingOdds: 2 },
+    { name: 'Midnight', bettingOdds: 2 },
+    { name: 'Misty', bettingOdds: 2 },
+  ]);
   return (
     <div>
       <div className='flex horseInfo'>
-        <h5>Horse Name</h5>
-        <h5>Betting Odds</h5>
-        <h5>Bet</h5>
+        <h5 className='horseInfo-title'>Horse Name</h5>
+        <h5 className='horseInfo-title'>Betting Odds</h5>
+        <h5 className='horseInfo-title'>Bet</h5>
       </div>
 
       <div className='flex horse'>
         <p className='nameWidth'>Sprite</p>
-        <p>{bettingOdds}</p>
-        <Betting />
+        <p>{bettingOdds[0].bettingOdds}</p>
+        <MakeBet />
       </div>
 
       <div className='flex horse'>
         <p className='nameWidth'>Thena</p>
-        <p>{bettingOdds}</p>
-        <Betting />
+        <p>{bettingOdds[0].bettingOdds}</p>
+        <MakeBet />
       </div>
 
       <div className='flex horse'>
         <p className='nameWidth'>Midnight</p>
-        <p>{bettingOdds}</p>
-        <Betting />
+        <p>{bettingOdds[0].bettingOdds}</p>
+        <MakeBet />
       </div>
 
       <div className='flex horse'>
         <p className='nameWidth'>Misty</p>
-        <p>{bettingOdds}</p>
-        <Betting />
+        <p>{bettingOdds[0].bettingOdds}</p>
+        <MakeBet />
       </div>
     </div>
   );
--- src/component/MakeBet.js
+++ src/component/MakeBet.js
@@ -1,12 +1,25 @@
 import React, { useState } from 'react';
 
 export const MakeBet = () => {
-  const [betting, setBetting] = useState(2);
+  const [horseBetting, setHorseBetting] = useState(0);
+
+  const increase = () => {
+    setHorseBetting((preValue) => preValue + 1);
+  };
+
+  const decrease = () => {
+    if (horseBetting < 1) return;
+    setHorseBetting((preValue) => preValue - 1);
+  };
   return (
     <div className='bettingFlex'>
-      <button className='button'>&#43;</button>
-      <p>{betting}</p>
-      <button className='button'>&minus;</button>
+      <button className='button' onClick={increase}>
+        &#43;
+      </button>
+      <p>{horseBetting}</p>
+      <button className='button' onClick={decrease}>
+        &minus;
+      </button>
     </div>
   );
 };
