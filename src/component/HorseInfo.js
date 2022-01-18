a283f64882ebd85ffdc850a8283e240a96161a83
yuling <yulinglin@protonmail.com>
2022-01-18 10:05:27 +0800

    refactor: Add parenthese html code in HorseInfo.js

--- src/component/HorseInfo.js
+++ src/component/HorseInfo.js
@@ -14,7 +14,9 @@ function HorseInfo() {
       <div className='flex horseInfo'>
         <h5 className='horseInfo-title'>Horse Name</h5>
         <h5 className='horseInfo-title'>Betting Odds</h5>
-        <h5 className='horseInfo-title'>Bet</h5>
+        <h5 className='horseInfo-title'>
+          Bet <span>&#40;</span>USD<span>&#41;</span>
+        </h5>
       </div>
 
       <div className='flex horse'>
