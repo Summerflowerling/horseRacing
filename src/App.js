dc23281b5eab4723a0d36eb5e9f4b30a173fd80b
yuling <yulinglin@protonmail.com>
2022-01-31 15:25:37 +0800

    refactor: Add Route for home page and the game display screen

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import './App.css';
 import { UserInfo } from './Component/UserInfo';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import History from './Component/History';
 import HorseInfo from './Component/HorseInfo';
 import AmountCalculate from './Component/AmountCalculate';
 import BetAmountConextProvider from './Context/BetAmountConext';
+import RaceGameDisplay from './Component/RaceGameDisplay';
 
 function App() {
   return (
-    <BetAmountConextProvider>
-      <UserInfo />
-      <History />
-      <HorseInfo />
-      <AmountCalculate />
-    </BetAmountConextProvider>
+    <>
+      <BetAmountConextProvider>
+        <Router>
+          <Switch>
+            <Route
+              path='/'
+              exact
+              render={(props) => (
+                <div>
+                  <UserInfo />
+                  <History />
+                  <HorseInfo />
+                  <AmountCalculate />
+                </div>
+              )}
+            />
+
+            <Route path='/race' component={RaceGameDisplay} />
+          </Switch>
+        </Router>
+      </BetAmountConextProvider>
+    </>
   );
 }
 
