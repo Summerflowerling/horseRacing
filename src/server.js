706b34540cba1cda8a3050052d1963dba64ee1a8
yuling <yulinglin@protonmail.com>
2022-01-31 15:27:22 +0800

    chore: Add react-router-dom

--- src/server.js
+++ src/server.js
@@ -1,15 +1,28 @@
-const port = 8099;
+const port = 8089;
+const baseUrl = 'https://api.exchangeratesapi.io/v1/latest?access_key=';
+
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 const app = express();
 require('dotenv').config();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hi There');
+app.get('/exchangeRate', (req, res) => {
+  axios
+    .get(baseUrl)
+    .then(function (response) {
+      // handle success
+      res.send(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
 });
 
 app.listen(port, () => {
   console.log(`server running on ${port}`);
+  console.log(process.env.REACT_APP_EXCHANGERATES_API_KEYS);
 });
