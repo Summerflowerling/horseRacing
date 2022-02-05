09fd05aa5f2f7856ec0c8dea31179c8e5a05ff0f
yuling <yulinglin@protonmail.com>
2022-02-06 01:24:43 +0800

    refactor: Add motion to animate 4 horses
    Problem: Need to make sure the randomTime array contains 4 items. There are only three items in the array sometimes.

--- src/component/RaceGameDisplay.js
+++ src/component/RaceGameDisplay.js
@@ -1,14 +1,70 @@
-import React from 'react';
+import React, { useLayoutEffect, useState, useRef } from 'react';
+import { motion } from 'framer-motion';
 
 const RaceGameDisplay = () => {
+  const [start, setStart] = useState(false);
+  const [randomTime, setRandomTime] = useState(0);
+  const defaultTime = useRef(1.5);
+  const [animateY, setAnimateY] = useState('90vh');
+
+  useLayoutEffect(() => {
+    let randomArray = [];
+    let x = 0;
+    while (x < 10) {
+      let randomNum = Math.floor(Math.random(0, 1) * 5 + 2);
+      randomArray.push(randomNum);
+      x++;
+    }
+
+    const arrToSet = new Set(randomArray);
+    const uniqueArray = [...arrToSet];
+    setRandomTime(uniqueArray);
+    console.log('random array', randomArray);
+    console.log('final array', uniqueArray);
+    console.log('This is randomTime', randomTime);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  setTimeout(() => {
+    setStart(true);
+  }, 5000);
+
   return (
     <>
-      <div className='horse-flex'>
-        <div className='horseUni horse1'>Sprite</div>
-        <div className='horseUni horse2'>Thena</div>
-        <div className='horseUni horse3'>Midnight</div>
-        <div className='horseUni horse4'>Misty</div>
-      </div>
+      {start ? (
+        <div className='horse-flex'>
+          <motion.div
+            className='horseUni horse1'
+            animate={{ y: animateY }}
+            transition={{ duration: randomTime[0] }}
+          >
+            Sprite
+          </motion.div>
+          <motion.div
+            className='horseUni horse2'
+            animate={{ y: animateY }}
+            transition={{ duration: randomTime[1] }}
+          >
+            Thena
+          </motion.div>
+          <motion.div
+            className='horseUni horse3'
+            animate={{ y: animateY }}
+            transition={{ duration: randomTime[2] | defaultTime }}
+          >
+            Midnight
+          </motion.div>
+          <motion.div
+            className='horseUni horse4'
+            animate={{ y: animateY }}
+            transition={{ duration: randomTime[3] | defaultTime }}
+          >
+            Misty
+          </motion.div>
+        </div>
+      ) : (
+        <p>Not yet</p>
+      )}
+
       <div className='finishLine'>Finish Line</div>
     </>
   );
